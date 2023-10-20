93e9a292723a8498f069bb102bdccffa2f43a761
Anshika2608 <anshika2210142@akgec.ac.in>
2023-10-20 15:25:28 +0530

    notes app added

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./footer";
+import AddNote from "./AddNote";
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      
+      <AddNote />
+       <Footer />
+    </div>
+  );
+}
+
+export default App;
